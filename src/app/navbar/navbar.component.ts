5da42f107f5bbb28bebb109f1155337a2c336406
Tasheem Hargrove <tasheem@Tasheems-MBP.attlocal.net>
2021-12-24 02:00:17 -0600

    Render navbar dynamically depending on whether a user is logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public isLoggedIn = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Check logged in status anytime there is a change.
+    this.checkStatus();
   }
 
+  // Method used to update whether a user is signed in.
+  public checkStatus(): void {
+    const token = window.sessionStorage.getItem('Token');
+    if(token != null)
+      this.isLoggedIn = true;
+    else
+      this.isLoggedIn = false;
+  }
 }
